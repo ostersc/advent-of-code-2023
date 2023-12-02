5938b469612a18e1aad8f7653ce6da221fb21876
ostersc <ostersc@gmail.com>
2023-12-02 12:22:20 -0500

    day 2 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -6,8 +6,8 @@ fun main() {
         }
     }
 
-    fun isGamePossible(game: String, redLimit: Int, greenLimit: Int, blueLimit: Int): Boolean {
-        return game.split("; ").all { subset ->
+    fun isGamePossible(sets: String, redLimit: Int, greenLimit: Int, blueLimit: Int): Boolean {
+        return sets.split("; ").all { subset ->
             val cubes = parseCubes(subset)
             cubes.getOrDefault("red", 0) <= redLimit &&
                     cubes.getOrDefault("green", 0) <= greenLimit &&
@@ -27,8 +27,18 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        // Part 2 implementation goes here
-        return input.size
+        return input.sumOf { game ->
+            val (_, sets) = game.removePrefix("Game ").split(": ")
+            val cubesInGame = sets.split("; ").fold(mapOf<String, Int>()) { acc, subset ->
+                val cubes = parseCubes(subset)
+                mapOf(
+                    "red" to maxOf(acc.getOrDefault("red", 0), cubes.getOrDefault("red", 0)),
+                    "green" to maxOf(acc.getOrDefault("green", 0), cubes.getOrDefault("green", 0)),
+                    "blue" to maxOf(acc.getOrDefault("blue", 0), cubes.getOrDefault("blue", 0))
+                )
+            }
+            cubesInGame.values.fold(1) { acc, value -> acc * value }.toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
@@ -40,11 +50,12 @@ fun main() {
         "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
     )
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     // Uncomment and use these when real input is available
      val input = readInput("Day02")
      println("Part 1 result: ${part1(input)}")
-    // println("Part 2 result: ${part2(input)}")
+     println("Part 2 result: ${part2(input)}")
 }
 
 // You may need to implement readInput function if it's not already defined
