69bd314fb62afa65ffb4b1cce7edc4776ef9a834
ostersc <ostersc@gmail.com>
2023-12-03 09:40:48 -0500

    day 3 part 2 solved

--- src/Day03.kt
+++ src/Day03.kt
@@ -44,7 +44,48 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sumGearRatios = 0
+
+        fun extractNumber(x: Int, y: Int): Int? {
+            if (input[y][x] !in '0'..'9') return null
+            var numStr = ""
+            var i = x
+            while (i >= 0 && input[y][i] in '0'..'9') {
+                numStr = input[y][i] + numStr
+                i--
+            }
+            i = x + 1
+            while (i < input[y].length && input[y][i] in '0'..'9') {
+                numStr += input[y][i]
+                i++
+            }
+            return numStr.toIntOrNull()
+        }
+
+        for (y in input.indices) {
+            for (x in input[y].indices) {
+                if (input[y][x] == '*') {
+                    val adjacentNumbers = mutableListOf<Int>()
+                    for (dx in -1..1) {
+                        for (dy in -1..1) {
+                            if (dx == 0 && dy == 0) continue
+                            val nx = x + dx
+                            val ny = y + dy
+                            if (ny in input.indices && nx in input[ny].indices && input[ny][nx].isDigit()) {
+                                extractNumber(nx, ny)?.let {
+                                    if (!adjacentNumbers.contains(it)) adjacentNumbers.add(it)
+                                }
+                            }
+                        }
+                    }
+                    if (adjacentNumbers.size == 2) {
+                        sumGearRatios += adjacentNumbers[0] * adjacentNumbers[1]
+                    }
+                }
+            }
+        }
+
+        return sumGearRatios
     }
 
     // Test with the example input
@@ -61,8 +102,9 @@ fun main() {
         ".664.598.."
     )
     check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
 
-    // Read input from a file
+    // Read input from a file and compute results
     val input = readInput("Day03")
     println("Part 1 result: ${part1(input)}")
     println("Part 2 result: ${part2(input)}")
