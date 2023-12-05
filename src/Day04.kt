dff8b3c68fa874fdd5c064c04eaefb7f9d916873
ostersc <ostersc@gmail.com>
2023-12-04 19:49:09 -0500

    day 4 part 2 solved

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,17 +1,31 @@
 fun main() {
+    fun countMatches(card: String): Int {
+        val (winningNumbers, playerNumbers) = card.substringAfter(": ")
+            .split(" | ")
+            .map { numbers -> numbers.split(" ").filter(String::isNotEmpty).map(String::toInt) }
+        return playerNumbers.count { it in winningNumbers }
+    }
+
     fun part1(input: List<String>): Int {
         return input.map { card ->
-            val (winningNumbers, playerNumbers) = card.substringAfter(": ")
-                .split(" | ")
-                .map { numbers -> numbers.split(" ").filter(String::isNotEmpty).map(String::toInt) }
-            playerNumbers.filter { it in winningNumbers }.fold(0) { acc, _ -> if (acc == 0) 1 else acc * 2 }
+            val matches = countMatches(card)
+            if (matches > 0) 1.shl(matches - 1) else 0 // Use bitwise shift for doubling
         }.sum()
     }
 
-
     fun part2(input: List<String>): Int {
-        // Placeholder for Part 2 solution
-        return input.size
+        val cardCounts = MutableList(input.size) { 1 } // Start with one copy of each card
+
+        for (i in input.indices) {
+            val matches = countMatches(input[i])
+            for (j in 1..matches) {
+                if (i + j < input.size) {
+                    cardCounts[i + j] += cardCounts[i]
+                }
+            }
+        }
+
+        return cardCounts.sum()
     }
 
     // Test case
@@ -24,6 +38,8 @@ fun main() {
         "31 18 13 56 72 | 74 77 10 23 35 67 36 11"
     )
     check(part1(testInput) == 13)
+    check(part2(testInput) == 30)
+
     // Uncomment and test with real input
      val input = readInput("Day04")
      println("Part 1 result: ${part1(input)}")
