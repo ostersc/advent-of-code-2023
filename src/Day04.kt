761b349dbed97ddb7211d28e6d7a97a282437426
ostersc <ostersc@gmail.com>
2023-12-04 19:41:50 -0500

    day 4 part 1 solved

---
+++ src/Day04.kt
@@ -0,0 +1,31 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.map { card ->
+            val (winningNumbers, playerNumbers) = card.substringAfter(": ")
+                .split(" | ")
+                .map { numbers -> numbers.split(" ").filter(String::isNotEmpty).map(String::toInt) }
+            playerNumbers.filter { it in winningNumbers }.fold(0) { acc, _ -> if (acc == 0) 1 else acc * 2 }
+        }.sum()
+    }
+
+
+    fun part2(input: List<String>): Int {
+        // Placeholder for Part 2 solution
+        return input.size
+    }
+
+    // Test case
+    val testInput = listOf(
+        "41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        " 1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+    )
+    check(part1(testInput) == 13)
+    // Uncomment and test with real input
+     val input = readInput("Day04")
+     println("Part 1 result: ${part1(input)}")
+     println("Part 2 result: ${part2(input)}")
+}
