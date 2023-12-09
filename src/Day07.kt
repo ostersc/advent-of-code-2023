198e64c04b28d7de75a597fbe5e402fbf84dbd35
ostersc <ostersc@gmail.com>
2023-12-09 09:43:05 -0500

    Day 7.2 failed

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,57 +1,115 @@
+import java.io.File
+
+enum class HandType(val strength: Int) {
+    FIVE_OF_A_KIND(7),
+    FOUR_OF_A_KIND(6),
+    FULL_HOUSE(5),
+    THREE_OF_A_KIND(4),
+    TWO_PAIR(3),
+    ONE_PAIR(2),
+    HIGH_CARD(1)
+}
+
+data class Hand(val cards: String, val bid: Int, var rank: Int = 0)
+
 fun main() {
-    fun rankHand(hand: String): Int {
-        val ranks = mapOf('2' to 2, '3' to 3, '4' to 4, '5' to 5, '6' to 6, '7' to 7, '8' to 8, '9' to 9, 'T' to 10, 'J' to 11, 'Q' to 12, 'K' to 13, 'A' to 14)
-        val cards = hand.map { ranks[it] ?: 0 }
-        val counts = cards.groupingBy { it }.eachCount()
+    val hands = readHands(readInput("Day07"))
+//    val rankedHandsPart1 = rankHands(hands, false)
+//    val totalWinningsPart1 = calculateTotalWinnings(rankedHandsPart1)
+//    println("Part 1 - Total winnings: $totalWinningsPart1")
 
-        return when {
-            counts.containsValue(5) -> 8
-            counts.containsValue(4) -> 7
-            counts.containsValue(3) && counts.containsValue(2) -> 6
-            counts.containsValue(3) -> 5
-            counts.count { it.value == 2 } == 2 -> 4
-            counts.containsValue(2) -> 3
-            else -> 2
-        }
+    val rankedHandsPart2 = rankHands(hands, true)
+    val totalWinningsPart2 = calculateTotalWinnings(rankedHandsPart2)
+    println("Part 2 - Total winnings: $totalWinningsPart2")
+}
+
+fun readHands(lines: List<String>): List<Hand> {
+    return lines.map { line ->
+        val (hand, bid) = line.split(" ")
+        Hand(hand, bid.toInt())
+    }
+}
+
+fun rankHands(hands: List<Hand>, useJokerRule: Boolean): List<Hand> {
+    return hands
+        .sortedWith(Comparator { h1, h2 -> compareHands(h1, h2, useJokerRule) })
+        .mapIndexed { index, hand -> hand.copy(rank = index + 1) }
+}
+
+fun compareHands(h1: Hand, h2: Hand, useJokerRule: Boolean): Int {
+    val type1 = h1.handType(useJokerRule)
+    val type2 = h2.handType(useJokerRule)
+    if (type1 != type2) return type2.strength - type1.strength
+
+    val sortedCards1 = h1.sortedCards(useJokerRule)
+    val sortedCards2 = h2.sortedCards(useJokerRule)
+    sortedCards1.zip(sortedCards2).forEach { (c1, c2) ->
+        if (c1 != c2) return cardStrength(c2, useJokerRule) - cardStrength(c1, useJokerRule)
     }
 
-    val handComparator = Comparator<Pair<String, Int>> { a, b ->
-        val rankComparison = b.second.compareTo(a.second)
-        if (rankComparison != 0) return@Comparator rankComparison
-
-        val aCards = a.first.map { it -> "23456789TJQKA".indexOf(it) }
-        val bCards = b.first.map { it -> "23456789TJQKA".indexOf(it) }
-        for (i in 0 until 5) {
-            val cardComparison = bCards[i].compareTo(aCards[i])
-            if (cardComparison != 0) return@Comparator cardComparison
-        }
-        0
+    return 0
+}
+fun Hand.handType(useJokerRule: Boolean): HandType {
+    if (!useJokerRule) {
+        return determineHandType(this.cards.groupBy { it }.mapValues { it.value.size })
     }
 
-    fun part1(input: List<String>): Int {
-        val hands = input.map { it.split(" ") }
-        val rankedHands = hands.map { it[0] to rankHand(it[0]) }
-        val sortedHands = rankedHands.sortedWith(handComparator)
+    // Handling joker rule
+    val bestHandType = generatePossibleHands().maxOf { it.handType(useJokerRule = false) }
+    println("Optimized hand ${this.cards} with jokers to ${bestHandType}")
+    return bestHandType
+}
 
-        // Debug output
-        sortedHands.forEachIndexed { index, pair ->
-            println("Hand: ${pair.first}, Rank: ${pair.second}, Position: ${sortedHands.size - index}")
-        }
+fun Hand.generatePossibleHands(): List<Hand> {
+    val possibleHands = mutableListOf<Hand>()
+    val jokerCount = this.cards.count { it == 'J' }
 
-        return sortedHands.mapIndexed { index, pair -> hands.find { it[0] == pair.first }!![1].toInt() * (sortedHands.size - index) }.sum()
+    if (jokerCount == 0) {
+        possibleHands.add(this)
+        return possibleHands
     }
 
-    // Test input
-    val testInput = listOf(
-        "32T3K 765",
-        "T55J5 684",
-        "KK677 28",
-        "KTJJT 220",
-        "QQQJA 483"
-    )
-    check(part1(testInput) == 6440)
+    val nonJokerCards = this.cards.filter { it != 'J' }
+    generateCombinations(jokerCount).forEach { combination ->
+        possibleHands.add(Hand(nonJokerCards + combination, this.bid))
+    }
 
-    // Read the full input from the file
-    val input = readInput("Day07")
-    println("Part 1 result: ${part1(input)}")
+    return possibleHands
+}
+
+fun generateCombinations(jokerCount: Int): List<String> {
+    val combinations = mutableListOf<String>()
+    val allCards = "AKQJT98765432"
+
+    // Generate all combinations of joker replacements
+    allCards.forEach { card ->
+        combinations.add(card.toString().repeat(jokerCount))
+    }
+
+    return combinations
+}
+
+fun determineHandType(counts: Map<Char, Int>): HandType {
+    return when {
+        counts.any { it.value == 5 } -> HandType.FIVE_OF_A_KIND
+        counts.any { it.value == 4 } -> HandType.FOUR_OF_A_KIND
+        counts.size == 2 && counts.values.sorted() == listOf(2, 3) -> HandType.FULL_HOUSE
+        counts.any { it.value == 3 } -> HandType.THREE_OF_A_KIND
+        counts.size == 3 && counts.values.count { it == 2 } == 2 -> HandType.TWO_PAIR
+        counts.size == 4 -> HandType.ONE_PAIR
+        else -> HandType.HIGH_CARD
+    }
+}
+
+fun Hand.sortedCards(useJokerRule: Boolean): List<Char> {
+    return this.cards.map { if (it == 'J' && useJokerRule) '2' else it }
+        .sortedWith(Comparator { c1, c2 -> cardStrength(c2, useJokerRule) - cardStrength(c1, useJokerRule) })
+}
+
+fun cardStrength(card: Char, useJokerRule: Boolean): Int {
+    val order = "AKQJT98765432"
+    return if (useJokerRule && card == 'J') -1 else order.indexOf(card)
+}
+fun calculateTotalWinnings(hands: List<Hand>): Int {
+    return hands.sumOf { it.bid * it.rank }
 }
