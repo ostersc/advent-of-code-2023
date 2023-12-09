2bbff4a193c1a6c9e2598f0fb779db977f482f48
ostersc <ostersc@gmail.com>
2023-12-09 13:19:22 -0500

    Day 9 passed

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun computeNextValue(history: List<Int>): Int {
+    fun computeDifferences(history: List<Int>): List<List<Int>> {
         val sequences = mutableListOf<List<Int>>()
         var current = history
 
@@ -10,34 +10,46 @@ fun main() {
             current = differences
         }
 
-        println("Sequences for history $history: $sequences")
+        return sequences
+    }
 
+    fun extrapolateNextValue(history: List<Int>, sequences: List<List<Int>>): Int {
         var extrapolated = history.last()
         for (sequence in sequences) {
             extrapolated += sequence.lastOrNull() ?: 0
         }
-
-        println("Next value for history $history: $extrapolated")
         return extrapolated
     }
 
     fun part1(input: List<String>): Int {
-        return input.sumOf { computeNextValue(it.split(" ").map(String::toInt)) }
+        return input.sumOf {
+            val history = it.split(" ").map(String::toInt)
+            val sequences = computeDifferences(history)
+            extrapolateNextValue(history, sequences)
+        }
     }
 
-    // Sample input from the problem description
+    fun part2(input: List<String>): Int {
+        return input.sumOf {
+            val history = it.split(" ").map(String::toInt).reversed()
+            val sequences = computeDifferences(history)
+            extrapolateNextValue(history, sequences)
+        }
+    }
+
+    // Test input from the problem description
     val testInput = listOf(
         "0 3 6 9 12 15",
         "1 3 6 10 15 21",
         "10 13 16 21 30 45"
     )
 
-    // Expected result from the problem description
+    // Check for part1 and part2
     check(part1(testInput) == 114)
+    check(part2(testInput) == 2)
 
     // Read input (this function is assumed to be implemented)
      val input = readInput("Day09")
      println("Part 1 result: ${part1(input)}")
+     println("Part 2 result: ${part2(input)}")
 }
-
-// Uncomment these lines when the readInput function is available.
